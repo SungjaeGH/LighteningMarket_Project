b71afa5a4eaccee73d413d8a33711ebd75634fb5
sungjae <SungjaeGH.github.io>
2021-01-16 20:29:42 +0900

    DB ip변겅

--- src/test/java/subpackage/MySQLConnectionTest.java
+++ src/test/java/subpackage/MySQLConnectionTest.java
@@ -8,7 +8,7 @@ import java.sql.DriverManager;
 public class MySQLConnectionTest {
 
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String URL = "jdbc:mysql://121.151.52.240:3306/bunjang?useSSL=false&serverTimezone=Asia/Seoul&allowPublicKeyRetrieval=true";
+    private static final String URL = "jdbc:mysql://210.123.132.237:3306/bunjang?useSSL=false&serverTimezone=Asia/Seoul&allowPublicKeyRetrieval=true";
     private static final String USER = "sungjae";
     private static final String PASSWORD = "1234";
 
