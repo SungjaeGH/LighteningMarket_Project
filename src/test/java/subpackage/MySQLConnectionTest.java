734d48656e03fbaf1f566273571582b0f5aaec69
harryqas <harryqas94@gmail.com>
2021-01-20 14:37:12 +0900

    김동현(bootstrap 추가, home.jsp, footer.jsp, head.jsp 추가 및 변경) 01 20 14:35

--- src/test/java/subpackage/MySQLConnectionTest.java
+++ src/test/java/subpackage/MySQLConnectionTest.java
@@ -8,9 +8,9 @@ import java.sql.DriverManager;
 public class MySQLConnectionTest {
 
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String URL = "jdbc:mysql://210.123.132.237:3306/bunjang?useSSL=false&serverTimezone=Asia/Seoul&allowPublicKeyRetrieval=true";
-    private static final String USER = "sungjae";
-    private static final String PASSWORD = "1234";
+    private static final String URL = "jdbc:mysql://localhost:3306/bunjang?useSSL=false&serverTimezone=Asia/Seoul&allowPublicKeyRetrieval=true";
+    private static final String USER = "root";
+    private static final String PASSWORD = "rlaehdgus12!";
 
     @Test
     public void testConnection() throws Exception{
